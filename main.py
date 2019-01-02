71daf242b83c11586fb76d6a22ade169ec9f421b
z_goodman <zgoodman@live.unc.edu>
2019-01-01 19:36:45 -0500

    created a world map and a tiny astronaut picture to represent the ISS' location above the planet

--- main.py
+++ main.py
@@ -1,14 +1,49 @@
 import json
-import turtle
 import urllib.request
+import turtle
 
 # creates a variable that stores a url to a web service that displays ISS astronauts and their locations
-url = 'http://api.open-notify.org/astros.json'
+url_people = 'http://api.open-notify.org/astros.json'
 
-# calls the web service and saves the JSON data called as response
-response = urllib.request.urlopen(url)
+# calls the web service and saves the JSON data as response
+json_ppl_data = urllib.request.urlopen(url_people)
 
 # translates the JSON data to a Python data structure
-result = json.loads(response.read())
+ppl_in_space = json.loads(json_ppl_data.read())
 
-print(result)
+people = ppl_in_space['people']
+
+# print(ppl_in_space['number'], 'people in space')
+#
+# for p in people:
+#     print(p['name'], 'in ', p['craft'])
+
+url_iss = 'http://api.open-notify.org/iss-now.json'
+
+json_iss_data = urllib.request.urlopen(url_iss)
+
+iss_location = json.loads(json_iss_data.read())
+
+location = iss_location['iss_position']
+lon = location['longitude']
+lat = location['latitude']
+
+# creates a turtle object, same as tkinter's windows
+screen = turtle.Screen()
+
+# sets the size of the window in pixels
+screen.setup(1000, 610)
+
+# coordinates for the map of the world being used as the image,
+# starts as bottom left (x, y) and then top right (x, y)
+screen.setworldcoordinates(-180, -90, 180, 90)
+screen.bgpic('map.gif')
+
+# registers a turtle object similar to a drawing/shape
+screen.register_shape('iss_astro2.gif')
+iss = turtle.Turtle()
+iss.shape('iss_astro2.gif')
+iss.setheading(90)
+
+# same as tkinter's root.mainloop()
+turtle.done()
